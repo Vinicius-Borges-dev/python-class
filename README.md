e1ced8adb5b8a606d8fb7eae13a1e58374332615
VINICIUS OLIVEIRA BORGES <borges.2970@aluno.pr.senac.br>
2024-06-24 17:13:02 -0300

    Aprendendo a usar tramplate string

---
+++ README.md
@@ -0,0 +1,1 @@
+# Este repositório está sob os termos da licença GPL 3.0
---
+++ index.py
@@ -0,0 +1,19 @@
+print("Hello world")
+
+# for i in range(0, 10):
+#     print("Ola mundo vez:", i)
+
+
+# campo = int(input("Digite um número:")
+
+# print(campo)
+
+print("Vinícius")
+print("Moro aqui ó 👉🌍")
+print(21)
+
+# Input para escrever o nome
+name = input("Digíte seu nome:")
+print(f"Olá, {name}")
+print("Olá, {}".format(name))
+
