84f682eb509c6427daca7262ab9befdd1d5abb93
VINICIUS OLIVEIRA BORGES <borges.2970@aluno.pr.senac.br>
2024-06-24 17:20:08 -0300

    Declarando variáveis e usando em templates strings

--- index.py
+++ index.py
@@ -2,18 +2,24 @@ print("Hello world")
 
 # for i in range(0, 10):
 #     print("Ola mundo vez:", i)
-
-
 # campo = int(input("Digite um número:")
-
 # print(campo)
 
-print("Vinícius")
-print("Moro aqui ó 👉🌍")
-print(21)
+# print("Vinícius")
+# print("Moro aqui ó 👉🌍")
+# print(21)
 
 # Input para escrever o nome
-name = input("Digíte seu nome:")
-print(f"Olá, {name}")
-print("Olá, {}".format(name))
+# name = input("Digíte seu nome:")
+# print(f"Olá, {name}")
+# print("Olá, {}".format(name))
 
+nome = "Vinícius"
+idade = 21
+skills = ["frontend development", "backend development", "Design UX/UI"]
+salarioMin = 15000
+SalarioMax = "todos os"
+
+print(f"Olá, meu nome é {nome}, tenho {idade}")
+print(f"minhas habilidades são: {skills}")
+print(f"Procuro um emprego com um salário de {salarioMin} a {SalarioMax} reais")
