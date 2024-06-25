5a3c3311cc16ebfda34087a25fb686448bf0f3a7
VINICIUS OLIVEIRA BORGES <borges.2970@aluno.pr.senac.br>
2024-06-25 14:15:34 -0300

    Atividade 1

--- index.py
+++ index.py
@@ -1,25 +1,36 @@
-print("Hello world")
+def aulaPrint():
+    print("Hello world")
 
-# for i in range(0, 10):
-#     print("Ola mundo vez:", i)
-# campo = int(input("Digite um número:")
-# print(campo)
+def testeComFor():
+    for i in range(0, 10):
+        print("Ola mundo vez:", i)
 
-# print("Vinícius")
-# print("Moro aqui ó 👉🌍")
-# print(21)
+def aulaMeusDados():
+    print("Vinícius")
+    print("Moro aqui ó 👉🌍")
+    print(21)
 
-# Input para escrever o nome
-# name = input("Digíte seu nome:")
-# print(f"Olá, {name}")
-# print("Olá, {}".format(name))
+def aulaInputs():
+    # Input para escrever o nome
+    name = input("Digíte seu nome:")
+    print(f"Olá, {name}")
+    print("Olá, {}".format(name))
 
-nome = "Vinícius"
-idade = 21
-skills = ["frontend development", "backend development", "Design UX/UI"]
-salarioMin = 15000
-SalarioMax = "todos os"
+def aulaPratica():
+    nome = "Vinícius"
+    idade = 21
+    skills = ["frontend development", "backend development", "Design UX/UI"]
+    salarioMin = 15000
+    SalarioMax = "todos os"
 
-print(f"Olá, meu nome é {nome}, tenho {idade}")
-print(f"minhas habilidades são: {skills}")
-print(f"Procuro um emprego com um salário de {salarioMin} a {SalarioMax} reais")
+    print(f"Olá, meu nome é {nome}, tenho {idade} anos\nMinhas habilidades são: {skills}\nProcuro um emprego com um salário de {salarioMin} a {SalarioMax} euros por mês")
+
+aulaPratica()
+
+def atividade1():
+    name = input("Digite seu nome:")
+    apelido = input("Digite seu apelido:")
+    adress = input("Digite seu endereço:")
+    cityAndZip = input("Digite sua cidade e código postal:")
+    
+    print(f"Nome próprio{name}\nApelido: {apelido}\nEndereço: {adress}\nCidade e código postal: {cityAndZip}\n{name} {apelido} {adress} {cityAndZip}")
