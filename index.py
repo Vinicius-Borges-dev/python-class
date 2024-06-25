565b9338ba8f7b686817bb941f24ba719bf8f1a8
VINICIUS OLIVEIRA BORGES <borges.2970@aluno.pr.senac.br>
2024-06-25 14:30:10 -0300

    Atvidade 2

--- index.py
+++ index.py
@@ -22,15 +22,72 @@ def aulaPratica():
     skills = ["frontend development", "backend development", "Design UX/UI"]
     salarioMin = 15000
     SalarioMax = "todos os"
-
     print(f"Olá, meu nome é {nome}, tenho {idade} anos\nMinhas habilidades são: {skills}\nProcuro um emprego com um salário de {salarioMin} a {SalarioMax} euros por mês")
-
-aulaPratica()
 
 def atividade1():
     name = input("Digite seu nome:")
     apelido = input("Digite seu apelido:")
     adress = input("Digite seu endereço:")
     cityAndZip = input("Digite sua cidade e código postal:")
-    
-    print(f"Nome próprio{name}\nApelido: {apelido}\nEndereço: {adress}\nCidade e código postal: {cityAndZip}\n{name} {apelido} {adress} {cityAndZip}")
+    print(f"Nome próprio: {name}\nApelido: {apelido}\nEndereço: {adress}\nCidade e código postal: {cityAndZip}\n{name} {apelido} {adress} {cityAndZip}")
+
+# atividade1()
+
+def atividade2():
+    name = input("Digite um nome:")
+    year = input("Digite um ano:")
+    print(f"{name} é uma valente cavaleira, nascida no ano {year}. Certa manhã, {name} acordou com um barulho terrivel: um dração se aproximava da aldeia. Somente {name} poderia salvar os moradores da aldeia")
+
+# atividade2()
+
+def atividade3():
+    s
+
+
+def atividade4():
+    s
+
+
+def atividade5():
+    s
+
+
+def atividade6():
+    s
+
+
+def atividade7():
+    s
+
+
+def atividade8():
+    s
+
+
+def atividade9():
+    s
+
+
+def atividade10():
+    s
+
+
+def atividade11():
+    s
+
+
+def atividade12():
+    s
+
+
+def atividade13():
+    s
+
+
+def atividade14():
+    s
+
+
+def atividade15():
+    s
+
