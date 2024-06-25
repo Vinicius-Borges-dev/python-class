7d03505cf3a898ce37f9851ec4d00196bf0f7873
VINICIUS OLIVEIRA BORGES <borges.2970@aluno.pr.senac.br>
2024-06-25 15:00:24 -0300

    Atividades 3 e 4

--- index.py
+++ index.py
@@ -38,14 +38,23 @@ def atividade2():
     year = input("Digite um ano:")
     print(f"{name} é uma valente cavaleira, nascida no ano {year}. Certa manhã, {name} acordou com um barulho terrivel: um dração se aproximava da aldeia. Somente {name} poderia salvar os moradores da aldeia")
 
-# atividade2()
 
 def atividade3():
-    s
+    city = "São Paulo"
+    print(f"Antes: {city}")
+    city = "Rio de Janeiro"
+    print(f"Depois: {city}")
 
 
 def atividade4():
-    s
+    varStr = "String"
+    print(f"Primera variável: {varStr}")
+    varInt = 10
+    print(f"Segunda variável: {varInt}")
+    varFloat = 10.3
+    print(f"Terceira variável: {varFloat}")
+    
+    print(f"Tipo da primeira: {type(varStr)}, tipo da segunda: {type(varInt)}, tipo da terceira: {type(varFloat)}")
 
 
 def atividade5():
@@ -91,3 +100,38 @@ def atividade14():
 def atividade15():
     s
 
+
+choice = int(input("Escolha uma atividade: de 1 a 15:"))
+
+if choice == 1:
+    atividade1()
+elif choice == 2:
+    atividade2()
+elif choice == 3:
+    atividade3()
+elif choice == 4:
+    atividade4()
+elif choice == 5:
+    atividade5()
+elif choice == 6:
+    atividade6()
+elif choice == 7:
+    atividade7()
+elif choice == 8:
+    atividade8()
+elif choice == 9:
+    atividade9()
+elif choice == 10:
+    atividade10()
+elif choice == 11:
+    atividade11()
+elif choice == 12:
+    atividade12()
+elif choice == 13:
+    atividade13()
+elif choice == 14:
+    atividade14()
+elif choice == 15:
+    atividade15()
+else:
+    print("Escolha inválida. Por favor, escolha um número de 1 a 15.")
